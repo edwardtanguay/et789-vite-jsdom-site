bce8a265fa76d8c5e911e0bfd106e4db551c831f
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-16 11:55:14 +0100

    component works

---
+++ src/components/Header.ts
@@ -0,0 +1,6 @@
+export const Header = () => {
+	return /*html*/ `
+<h1 class="text-2xl mb-3">Vite JS-DOM Site</h1>
+<p>Welcome to this site.</p>
+	`;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import './style.scss';
 import skills from './data/skills.json';
+import { Header } from './components/Header';
 
 skills.sort((a,b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1)
 
 document.querySelector<HTMLBodyElement>('#app')!.innerHTML = /*html*/ `
-<h1 class="text-2xl mb-3">Vite JS-DOM Site</h1>
-<p>Welcome to this site.</p>
+
+${Header()}
 
 <h2 class="text-xl mt-4 text-yellow-300">${skills.length} Web Developer Skills</h2>
 <ul class="list-disc ml-6">

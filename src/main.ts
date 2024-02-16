5e9084f3f21564405e7e1318edd08c47d7b5126d
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-16 11:32:45 +0100

    json file works

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import './style.scss'
+import './style.scss';
+import skills from './data/skills.json';
 
 document.querySelector<HTMLBodyElement>('#app')!.innerHTML = /*html*/ `
 <h1 class="text-2xl mb-3">Vite JS-DOM Site</h1>
 <p>Welcome to this site.</p>
+
+<h2 class="text-xl mt-4 text-yellow-300">${skills.length} Web Developer Skills</h2>
 <ul class="list-disc ml-6">
-	<li>one</li>
-	<li>two</li>
-	<li>three</li>
+	${skills.map(skill => {
+		return /*html*/ `
+			<li><span class="text-orange-400 font-semibold">${skill.name}</span> - ${skill.description}</li>
+		`;
+	}).join('')}
 </ul>
 `;

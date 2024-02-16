428d9a83fb474ec61b827efa0cf26ea8a691f5e3
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-16 10:56:12 +0100

    sass works

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import './style.css'
+import './style.scss'
 
-document.querySelector<HTMLBodyElement>('#app')!.innerHTML = `
+document.querySelector<HTMLBodyElement>('#app')!.innerHTML = /*html*/ `
 <h1>Vite JS-DOM Site</h1>
 <p>Welcome to this site.</p>
+<ul>
+	<li>one</li>
+	<li>two</li>
+	<li>three</li>
+</ul>
 `;

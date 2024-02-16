f07b75205a9cedc2cbaf485d5d7446ec00c7e466
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-16 10:31:35 +0100

    base site

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLBodyElement>('#app')!.innerHTML = `
 <h1>Vite JS-DOM Site</h1>
 <p>Welcome to this site.</p>
 `;

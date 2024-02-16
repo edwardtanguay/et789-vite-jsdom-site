1564ffd4b69624980b3da6dd7a94398bc13952fc
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-16 11:12:57 +0100

    tailwind installed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './style.scss'
 
 document.querySelector<HTMLBodyElement>('#app')!.innerHTML = /*html*/ `
-<h1>Vite JS-DOM Site</h1>
+<h1 class="text-2xl mb-3">Vite JS-DOM Site</h1>
 <p>Welcome to this site.</p>
-<ul>
+<ul class="list-disc ml-6">
 	<li>one</li>
 	<li>two</li>
 	<li>three</li>
